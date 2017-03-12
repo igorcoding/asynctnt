b4ac9593f87c7a66b23335ef2b040c124fe5258b
igorcoding <igorcoding@gmail.com>
2017-03-12 23:24:30 +0300

    fix

--- .ci/travis-build-docs.sh
+++ .ci/travis-build-docs.sh
@@ -42,8 +42,6 @@ cd ..
 
 # Clean out existing contents
 rm -rf out/**/* || exit 0
-rm -rf out/* || exit 0
-rm -rf out/.* || exit 0
 
 # Run our compile script
 doCompile
@@ -65,11 +63,14 @@ git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+set +x
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in .ci/deploy_key.enc -out deploy_key -d
+set -x
+
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
