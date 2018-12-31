d2a63602cb532ba26294138b75bbdc49e6465fa7
igorcoding <igorcoding@gmail.com>
2018-12-31 17:19:49 +0300

    Install tarantool 1.10 from brew directly

--- .ci/travis-before-install-osx.sh
+++ .ci/travis-before-install-osx.sh
@@ -7,6 +7,7 @@ if [[ "${TARANTOOL_VERSION}" != "none" ]]; then
     if [[ "${TARANTOOL_VERSION}" == "2_x" ]]; then
         brew install .ci/tarantool.rb --HEAD
     else
-        brew install .ci/tarantool.rb
+        brew install tarantool
+#        brew install .ci/tarantool.rb
     fi
 fi
